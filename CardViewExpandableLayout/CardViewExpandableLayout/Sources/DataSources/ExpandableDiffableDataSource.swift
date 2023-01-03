d76ad9900fc9fae87d6f5f31588d1c8271312e74
김태현 <taehyk528@gmail.com>
2023-01-04 00:35:00 +0900

    Add ExpandableDiffableDataSource

---
+++ CardViewExpandableLayout/CardViewExpandableLayout/Sources/DataSources/ExpandableDiffableDataSource.swift
@@ -0,0 +1,22 @@
+//
+//  ExpandableDiffableDataSource.swift
+//  CardViewExpandableLayout
+//
+//  Created by taekki on 2023/01/04.
+//
+
+import UIKit
+
+final class ExpandableDiffableDataSource: UICollectionViewDiffableDataSource<Int, ExpandableViewSection> {
+  init(collectionView: UICollectionView) {
+    super.init(collectionView: collectionView) { collectionView, indexPath, item -> UICollectionViewCell? in
+      guard let cell = collectionView.dequeueReusableCell(
+        withReuseIdentifier: ExpandableCell.identifier,
+        for: indexPath) as? ExpandableCell else {
+          fatalError("Could not cast cell as \(ExpandableCell.self)")
+        }
+      cell.item = item
+      return cell
+    }
+  }
+}
